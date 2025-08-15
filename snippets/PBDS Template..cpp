68c020a2f3f74b6c4fb3f84482c94dc3c30ffb6b
Al Kayes Rifat <157808289+alkayesrifat@users.noreply.github.com>
2025-08-15 15:03:33 +0600

    Update PBDS Template..cpp

--- snippets/PBDS Template..cpp
+++ snippets/PBDS Template..cpp
@@ -1,31 +1,20 @@
-//for set
 #include <bits/stdc++.h>
-
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp> // PBDS container
+#include <ext/pb_ds/tree_policy.hpp>     // Tree policies
 using namespace __gnu_pbds;
-
 using namespace std;
 
-template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// PBDS template (can be used as set or multiset)
+template <typename T>
+using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// Notes:
+// - Change 'pbds' to any name you like
+// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+// - Functions: order_of_key(x), *find_by_order(k)
 
-int main() {
-
-   return 0;
-}
-
-
-//  for multiset 
-
-#include <bits/stdc++.h>
-
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-
-using namespace std;
-
-template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define ull unsigned long long
+#define endl "\n"
 
 int main() {
 
