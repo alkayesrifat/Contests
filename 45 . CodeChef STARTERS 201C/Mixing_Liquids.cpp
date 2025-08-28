1c2d9f85f74f0fabd47af5e4bc2355145660efd1
alkayesrifat <alkayesrifat47@gmail.com>
2025-08-28 10:26:48 +0600

    201C added

---
+++ 45 . CodeChef STARTERS 201C/Mixing_Liquids.cpp
@@ -0,0 +1,47 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author     : Al Kayes Rifat
+ * Portfolio  : alkayesrifat.netlify.app
+ * Date       : 27/08/2025 (Wednesday)
+ * Time       : 20:31 
+ * ---------------------
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+#define ll long long int
+#define ull unsigned long long
+#define endl "\n"
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_case ;
+    cin >>  test_case ;
+    while(test_case--)
+    {
+        int a ,b;
+        cin >> a >> b;
+
+        int cnt = 0 ;
+
+        while (a >= 1 && b >= 2)
+        {
+            cnt+=3;
+            a--;
+            b-=2;
+        }
+        cout << cnt << endl;
+        
+
+    }
+
+    return 0;
+}
