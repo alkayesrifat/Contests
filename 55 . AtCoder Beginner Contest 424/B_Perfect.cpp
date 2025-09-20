27193664d23f4f7e4e663d3a940becfb17fbabf8
alkayesrifat <alkayesrifat47@gmail.com>
2025-09-20 20:18:23 +0600

    abc 424 added

---
+++ 55 . AtCoder Beginner Contest 424/B_Perfect.cpp
@@ -0,0 +1,57 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author     : Al Kayes Rifat
+ * Portfolio  : alkayesrifat.netlify.app
+ * Date       : 20/09/2025 (Saturday)
+ * Time       : 18:10 
+ * ---------------------
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+#define ll long long int
+#define ull unsigned long long
+#define endl "\n"
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n , m , k ;
+    cin >> n >> m >> k ;
+
+    map<int,int>mp;
+
+    vector<int>ans;
+
+    while (k--)
+    {
+        int a,  b;
+        cin >> a >> b ;
+        mp[a]++;
+        /* code */
+
+        if (mp[a]==m)
+        {
+            ans.push_back(a);
+            /* code */
+        }
+        
+
+    }
+
+    for(auto x : ans){
+        cout << x << " " ;
+    }
+
+
+    
+
+    return 0;
+}
