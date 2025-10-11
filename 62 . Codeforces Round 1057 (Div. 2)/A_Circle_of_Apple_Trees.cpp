0e53fc20d775c4362ba5751f99520078496508ff
alkayesrifat <alkayesrifat47@gmail.com>
2025-10-11 10:12:03 +0600

    cf round 1057 added

---
+++ 62 . Codeforces Round 1057 (Div. 2)/A_Circle_of_Apple_Trees.cpp
@@ -0,0 +1,46 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author     : Al Kayes Rifat
+ * Portfolio  : alkayesrifat.netlify.app
+ * Date       : 10/10/2025 (Friday)
+ * Time       : 20:37 
+ * ---------------------
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+#define ll long long int
+#define ull unsigned long long
+#define endl "\n"
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int alkayesrifat ;
+    cin >>  alkayesrifat ;
+    while(alkayesrifat--)
+    {
+        int n ;
+        cin >> n ;
+        set<int>st;
+
+        for (int i = 0; i < n; i++)
+        {
+            int val;
+            cin >> val;
+            st.insert(val);
+        }
+        cout << st.size() << endl;
+        
+
+    }
+
+    return 0;
+}
