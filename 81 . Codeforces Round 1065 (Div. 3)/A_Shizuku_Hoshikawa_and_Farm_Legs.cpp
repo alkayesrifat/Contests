cd0f5eb0cd402542d48934a83b1aeb0c6e8f0c26
alkayesrifat <alkayesrifat47@gmail.com>
2025-11-22 16:25:44 +0600

    cf round 1065 added

---
+++ 81 . Codeforces Round 1065 (Div. 3)/A_Shizuku_Hoshikawa_and_Farm_Legs.cpp
@@ -0,0 +1,43 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author     : Al Kayes Rifat
+ * Portfolio  : alkayesrifat.netlify.app
+ * Date       : 20/11/2025 (Thursday)
+ * Time       : 20:41 
+ * ---------------------
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+#define ll long long int
+#define ull unsigned long long
+#define endl "\n"
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int alkayesrifat ;
+    cin >>  alkayesrifat ;
+    while(alkayesrifat--)
+    {
+        ll n;
+        cin >> n;
+
+        if(n % 2 != 0){
+            cout << 0 << endl;
+            continue;
+        }
+
+        cout << (n / 4 ) + 1 << endl;
+
+    }
+
+    return 0;
+}
