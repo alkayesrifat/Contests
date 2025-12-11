bcfc0fd93d4bea7e54d0c04eeeb56158d2b6e6b7
alkayesrifat <alkayesrifat47@gmail.com>
2025-12-11 06:39:02 +0600

    cc 216C added

---
+++ 91 . CodeChef STARTERS 216C/Best_Seats.cpp
@@ -0,0 +1,48 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author     : Al Kayes Rifat
+ * Portfolio  : alkayesrifat.netlify.app
+ * Date       : 10/12/2025 (Wednesday)
+ * Time       : 20:36 
+ * ---------------------
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+#define ll long long int
+#define ull unsigned long long
+#define endl "\n"
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int alkayesrifat ;
+    cin >>  alkayesrifat ;
+    while(alkayesrifat--)
+    {
+        int n ;
+        cin >> n ;
+
+        vector<int>v(n);
+
+        for(int i = 0 ; i < n ; i++){
+            cin >> v[i];
+        }
+
+        int minn = INT_MAX;
+        for(int i = 0 ; i < n - 1 ; i++){
+            minn = min(minn , (v[i] +  v[i+1]) );
+        }
+        cout << minn << endl;
+
+    }
+
+    return 0;
+}
---
+++ 91 . CodeChef STARTERS 216C/Scoring.cpp
@@ -0,0 +1,40 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author     : Al Kayes Rifat
+ * Portfolio  : alkayesrifat.netlify.app
+ * Date       : 10/12/2025 (Wednesday)
+ * Time       : 20:32 
+ * ---------------------
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>// - Use 'less<T>' for set, 'less_equal<T>' for multiset
+#define ll long long int
+#define ull unsigned long long
+#define endl "\n"
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int alkayesrifat ;
+    cin >>  alkayesrifat ;
+    while(alkayesrifat--)
+    {
+        int a , b ;
+        cin >>a >> b ;
+
+        int aa = b - a ;
+
+        cout << a + (aa / 2) << " " << aa / 2 << endl;
+
+    }
+
+    return 0;
+}
